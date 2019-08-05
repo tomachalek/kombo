5b6c13253445d3e2842093a3cdbe55ea98fa6927
Tomas Machalek <tomas.machalek@gmail.com>
2019-08-05 11:49:47 +0200

    Fix interface

--- src/kombo/main.ts
+++ src/kombo/main.ts
@@ -78,6 +78,7 @@ export interface IActionDispatcher {
     dispatch<T extends Action|Observable<Action>>(action:T):void;
     registerStatefulModel<T>(model:StatefulModel<T>):Subscription;
     registerModel<T>(model:IStatelessModel<T>, initialState:T):BehaviorSubject<T>;
+    registerActionListener(fn:(action:Action)=>void):Subscription;
 }
 
 /**
